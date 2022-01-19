6c21948590cae8c19991a6e19b21005cef33bd8e
xiayundong <1032176134@qq.com>
2022-01-19 21:19:48 +0800

    1.0.1-1
    修改需求5

--- app/src/main/java/com/fenmenbielei/bedsense/base/BaseActivity.java
+++ app/src/main/java/com/fenmenbielei/bedsense/base/BaseActivity.java
@@ -32,7 +32,7 @@ public class BaseActivity extends AppCompatActivity {
         //初始化PreferenceUtil
         PreferenceUtil.init(this);
         //根据上次的语言设置，重新设置语言
-     //   switchLanguage(PreferenceUtil.getString("language", "zh"));
+        switchLanguage(PreferenceUtil.getString("language", "en"));
     }
 
     @Override
